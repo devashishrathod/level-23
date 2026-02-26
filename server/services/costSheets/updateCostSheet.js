0287933a5960b7c1291e08be95523b1b9201b6c6
Devashish Rathod <rathod.devashish09@gmail.com>
2026-02-26 14:00:54 +0530

    other charges added done

--- server/services/costSheets/updateCostSheet.js
+++ server/services/costSheets/updateCostSheet.js
@@ -10,20 +10,22 @@ exports.updateCostSheet = async (id, payload) => {
 
   payload = payload || {};
 
-  const { isActive } = payload;
+  const { otherCharges, isActive } = payload;
 
   if (typeof isActive !== "undefined") costSheet.isActive = isActive;
 
-  if (typeof costSheet.total === "undefined" || costSheet.total === null) {
-    costSheet.total =
-      (costSheet.basicRate || 0) +
-      (costSheet.development || 0) +
-      (costSheet.dgBackup || 0) +
-      (costSheet.recreation || 0) +
-      (costSheet.societyLegal || 0) +
-      (costSheet.floorRise || 0) +
-      (costSheet.otherCharges || 0);
+  if (typeof otherCharges !== "undefined") {
+    costSheet.otherCharges = otherCharges;
   }
+
+  costSheet.total =
+    (costSheet.basicRate || 0) +
+    (costSheet.development || 0) +
+    (costSheet.dgBackup || 0) +
+    (costSheet.recreation || 0) +
+    (costSheet.societyLegal || 0) +
+    (costSheet.floorRise || 0) +
+    (costSheet.otherCharges || 0);
 
   costSheet.updatedAt = new Date();
   await costSheet.save();
